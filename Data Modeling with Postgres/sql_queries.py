d07546b52ca8f8a61132e11c469b77ef99eb4942
willytakasawa <w.takasawa.fernandes@gmail.com>
2022-07-19 15:29:21 -0300

    adjusting elt notebook

--- Data Modeling with Postgres/sql_queries.py
+++ Data Modeling with Postgres/sql_queries.py
@@ -22,7 +22,7 @@ songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (
 """)
 
 user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
-    user_id INT PRIMARI KEY,
+    user_id INT PRIMARY KEY,
     first_name VARCHAR,
     last_name VARCHAR,
     gender CHAR(1),
@@ -31,7 +31,7 @@ user_table_create = ("""CREATE TABLE IF NOT EXISTS users (
 """)
 
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (
-    song_id INT PRIMARY KEY,
+    song_id VARCHAR PRIMARY KEY,
     title VARCHAR,
     artist_id VARCHAR REFERENCES artists (artist_id),
     year INT,
@@ -61,30 +61,50 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time (
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""INSERT INTO songplays () VALUES ()
+songplay_table_insert = ("""
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (songplay_id) DO NOTHING
 """)
 
-user_table_insert = ("""INSERT INTO users () VALUES ()
+user_table_insert = ("""
+    INSERT INTO users (user_id, first_name, last_name, gender, level)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (user_id) DO UPDATE SET level = EXCLUDED.level
 """)
 
-song_table_insert = ("""INSERT INTO songs () VALUES ()
+song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (song_id) DO NOTHING
 """)
 
-artist_table_insert = ("""INSERT INTO  artists () VALUES ()
+artist_table_insert = ("""
+    INSERT INTO  artists (artist_id, name, location, latitude, longitude)
+    VALUES (%s, %s, %s, %s, %s)
+    ON CONFLICT (artist_id) DO NOTHING
 """)
 
 
-time_table_insert = ("""INSERT INTO time () VALUES ()
+time_table_insert = ("""
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+    ON CONFLICT (start_time) DO NOTHING
 """)
 
 # FIND SONGS
 
+# %s represents values filled by the loop on elt.ipynb
 song_select = ("""
+    SELECT songs.song_id, artists.artist_id
+    FROM songs
+    JOIN artists ON songs.artist_id = artists.artist_id
+    WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s
 """)
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create,
-                        song_table_create, artist_table_create, time_table_create]
+create_table_queries = [time_table_create, artist_table_create, user_table_create,
+                        song_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop,
                       song_table_drop, artist_table_drop, time_table_drop]
